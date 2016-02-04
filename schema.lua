46d53c9ca85e2d244d59ace4e95f7aa16b4d44cc
Tim Caswell <tim@creationix.com>
2016-02-04 17:38:15 -0600

    Update homepage to new repo

--- schema.lua
+++ schema.lua
@@ -1,7 +1,7 @@
 --[[lit-meta
   name = "creationix/schema"
-  version = "0.2.0"
-  homepage = "https://github.com/virgo-agent-toolkit/super-agent/blob/master/libs/schema"
+  version = "0.2.1"
+  homepage = "https://github.com/creationix/lua-schema"
   description = "A runtime type-checking system to validate API functions."
   tags = {"schema", "type", "api"}
   license = "MIT"
